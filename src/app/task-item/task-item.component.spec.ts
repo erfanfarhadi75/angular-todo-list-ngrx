d8c4a1a330ffded40b209287ea82ea1d097afdd7
vahidbn <36687930+vahidbn@users.noreply.github.com>
2023-09-13 17:44:39 +0330

    test: implement Test for Notifications Action (#2)
    Co-authored-by: v.barzegar <v.barzegar@asax.ir>

--- src/app/task-item/task-item.component.spec.ts
+++ src/app/task-item/task-item.component.spec.ts
@@ -4,6 +4,7 @@ import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { TaskItemComponent } from './task-item.component';
 import { ITask } from '../model';
 import { AppState } from '../state/app.state';
+import { markAsNotificationsAction, removeFromNotificationsAction } from '../state/actions';
 
 describe('TaskItemComponent', () => {
   let component: TaskItemComponent;
@@ -32,7 +33,47 @@ describe('TaskItemComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create TaskItemComponent', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should render the AddToNotifications button when task.isNotification is false', ()=>{
+    component.task.isNotification = false ;
+    fixture.detectChanges();
+    const addButton = fixture.nativeElement.querySelector(
+      'button[aria-label="is notifications"]');
+    expect(addButton).toBeTruthy();
+  })
+
+  it('should render the RemoveFromNotifications button when task.isNotification is true', ()=>{
+    component.task.isNotification = true ;
+    fixture.detectChanges();
+    const removeButton =fixture.nativeElement.querySelector(
+      'button[aria-label="remove notifications"]');
+    expect(removeButton).toBeTruthy();
+  })
+
+
+  it('should dispatch  markAsNotificationsAction when clicking onAddToNotifications button', ()=>{
+    component.task.isNotification = false ;
+    fixture.detectChanges();
+    const addButton  = fixture.nativeElement.querySelector(
+      'button[aria-label="is notifications"]');
+    addButton.click();
+    expect(store.dispatch).toHaveBeenCalledWith(
+      markAsNotificationsAction({ id: component.task.id })
+    );
+  })
+
+  it('should dispatch removeFromNotificationsAction when clicking onRemoveFromNotifications Button', ()=>{
+    component.task.isNotification = true ;
+    fixture.detectChanges();
+    const removeButton = fixture.nativeElement.querySelector(
+      'button[aria-label="remove notifications"]');
+    removeButton.click();
+    expect(store.dispatch).toHaveBeenCalledWith(
+      removeFromNotificationsAction({id : component.task.id})
+    )
+  })
+
 });
